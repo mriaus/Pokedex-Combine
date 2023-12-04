a858cd1f9f5d11a3da8e478aaca2aedbf915cf3e
mriaus <55255215+mriaus@users.noreply.github.com>
2023-12-04 21:17:27 +0100

    Solved a problem in the decode and starting the pokemon list cell details

---
+++ PokedexCombine/Domain/Commons/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  PokedexCombine
+//
+//  Created by Marcos on 4/12/23.
+//
+
+import Foundation
+
+enum pokemonTypes {
+    case normal
+    case fighting
+    case flying
+    case poison
+    case ground
+    case rock
+    case bug
+    case ghost
+    case steel
+    case fire
+    case water
+    case grass
+    case electric
+    case psychic
+    case ice
+    case dragon
+    case dark
+    case fairy
+    case unknown
+    case shadow
+}
---
+++ PokedexCombine/Presentation/Views/PokemonList/PokemonListCellView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonListCellView.swift
+//  PokedexCombine
+//
+//  Created by Marcos on 4/12/23.
+//
+
+import SwiftUI
+
+struct PokemonListCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonListCellView()
+}
